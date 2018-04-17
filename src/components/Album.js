5d5debff7899811cfdbd1e2a31fc394fdd42fb2e
danielkim <danielkimcw@gmail.com>
2018-04-16 19:00:31 -0700

    assignment buttons done

--- src/components/Album.js
+++ src/components/Album.js
@@ -57,6 +57,14 @@
       this.play(newSong);
    }
 
+   handleNextClick(){
+     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song );
+     const newIndex = Math.min(4, currentIndex + 1);
+     const newSong = this.state.album.songs[newIndex];
+     this.setSong(newSong);
+     this.play(newSong);
+   }
+
    render() {
      return (
        <section className="album">
@@ -94,7 +102,8 @@
            isPlaying={this.state.isPlaying}
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-           handlePrevClick={() => this.handlePrevClick()}        
+           handlePrevClick={() => this.handlePrevClick()} 
+           handleNextClick={() => this.handleNextClick()}       
          />
        </section>
      );
