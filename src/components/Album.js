e772ebbca6423a6dead3c363e675e45d7369a4be
danielkim <danielkimcw@gmail.com>
2018-04-16 18:51:42 -0700

    checkpoint buttons done

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
  import React, { Component } from 'react';
  import albumData from './../data/albums';
+ import PlayerBar from './PlayerBar';
 
   class Album extends Component {
    constructor(props) {
@@ -43,9 +44,17 @@
      if (this.state.isPlaying && isSameSong) {
        this.pause();
      } else {
-       if (!isSameSong) { this.setSong(song); }    
-       this.play();
+       if (!isSameSong) { this.setSong(song) }    
+       this.play(song);
      }
+   }
+
+   handlePrevClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play(newSong);
    }
 
    render() {
@@ -81,6 +90,12 @@
           	}
            </tbody>
          </table>
+      	 <PlayerBar
+           isPlaying={this.state.isPlaying}
+           currentSong={this.state.currentSong}
+           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={() => this.handlePrevClick()}        
+         />
        </section>
      );
    }
