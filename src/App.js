370ff7a8417b7b964a55e99dee1ea7f92a4ac83e
danielkim <danielkimcw@gmail.com>
2018-04-17 18:18:37 -0700

    checkpoint styling 2

--- src/App.js
+++ src/App.js
@@ -8,19 +8,30 @@ import Album from './components/Album';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-         <header>
-           <nav>
-             <Link to='/'>Landing</Link>
-             <Link to='/library'>Library</Link>
-           </nav>
-           <h1>Bloc Jams</h1>
-         </header>
-         <main>
-           <Route exact path="/" component={Landing} />
-           <Route path="/library" component={Library} />
-           <Route path="/album/:slug" component={Album} />
-         </main>
+      <div className="mdl-layout mdl-js-layout">
+        <header className="mdl-layout__header header">
+          <div className="mdl-layout-icon"></div>
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout__title" style={{fontSize: "40px", fontWeight: "bold"}}><img className="logo" src="./../assets/images/bloc_jams_logo.png" alt="Bloc Jams logo" /></span>
+            <div className="mdl-layout-spacer"></div>
+            <nav className="mdl-navigation">
+              <Link to='/' className="mdl-navigation__link">Landing</Link>
+              <Link to='/library' className="mdl-navigation__link">Library</Link>
+            </nav>
+          </div>
+        </header>
+        <div className="mdl-layout__drawer">
+          <span className="mdl-layout__title" style={{fontSize: "40px", fontWeight: "bold"}}>Bloc Jams</span>
+          <nav className="mdl-navigation">
+            <Link to='/' className="mdl-navigation__link">Landing</Link>
+            <Link to='/library' className="mdl-navigation__link">Library</Link>
+          </nav>
+        </div>
+        <main className="mdl-layout__content">
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album/:slug" component={Album} />
+        </main>
       </div>
     );
   }
